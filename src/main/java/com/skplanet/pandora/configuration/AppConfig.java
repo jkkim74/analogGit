86c1711b1a3a6480a6a0097e6cfbb83d841de99d
PP68961 <1600328@partner.skcc.com>
2016-08-17 15:45:09 +0900

    트랜잭션 설정

--- src/main/java/com/skplanet/pandora/configuration/AppConfig.java
+++ src/main/java/com/skplanet/pandora/configuration/AppConfig.java
@@ -3,12 +3,12 @@ package com.skplanet.pandora.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan(basePackages = "com.skplanet.pandora")
-@EnableTransactionManagement
 @Import({ WebConfig.class, DataSourceConfig.class })
+@EnableScheduling
 public class AppConfig {
 
 }
--- src/main/java/com/skplanet/pandora/configuration/DataSourceConfig.java
+++ src/main/java/com/skplanet/pandora/configuration/DataSourceConfig.java
@@ -12,9 +12,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @PropertySource("classpath:/config/common.properties")
+@EnableTransactionManagement(proxyTargetClass = true)
 public class DataSourceConfig implements EnvironmentAware, ApplicationContextAware {
 
 	ApplicationContext applicationContext;
@@ -29,6 +33,11 @@ public class DataSourceConfig implements EnvironmentAware, ApplicationContextAwa
 	@Override
 	public void setEnvironment(Environment environment) {
 		this.env = environment;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(mysqlDataSource());
 	}
 
 	@Bean(destroyMethod = "close")
