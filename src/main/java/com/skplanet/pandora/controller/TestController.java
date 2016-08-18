18cba05f89d308cb4528073da2f8b397cc3e0cbf
PP68961 <1600328@partner.skcc.com>
2016-08-18 09:13:07 +0900

    import 누락

--- src/main/java/com/skplanet/pandora/controller/TestController.java
+++ src/main/java/com/skplanet/pandora/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skplanet.pandora.repository.mysql.MysqlRepository;
