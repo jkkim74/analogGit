3fd952dcafc72aa5f9b52e18cfe72230df7418b3
PP68961 <1600328@partner.skcc.com>
2016-08-31 12:11:26 +0900

    화면 수정

--- src/main/webapp/partials/pan0102/pan0102.js
+++ src/main/webapp/partials/pan0102/pan0102.js
@@ -1,5 +1,13 @@
 'use strict';
 
-App.controller('Pan0102Ctrl', ["$scope", function ($scope) {
+App.controller('Pan0102Ctrl', ["$scope", "$q", "$http", "$timeout", "$stateParams", "Upload", "uiGridConstants", "toastr", function ($scope, $q, $http, $timeout, $stateParams, Upload, uiGridConstants, toastr) {
+
   $scope.title = '고객 정보 및 장기 거래 실적 조회';
+
+  $scope.selectOptions = [
+    { label: '회원ID', value: 'mbrId' },
+    { label: '카드번호', value: 'cardNo' },
+    { label: 'OCB닷컴 로그인ID', value: 'ocbcomLgnId' }
+  ];
+
 }]);
