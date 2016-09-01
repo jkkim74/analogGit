abff1ea36b85a6b52593a060630e53999de415f9
PP68961 <1600328@partner.skcc.com>
2016-09-01 12:10:57 +0900

    설정변경

--- src/main/java/com/skplanet/pandora/common/Constant.java
+++ src/main/java/com/skplanet/pandora/common/Constant.java
@@ -2,7 +2,7 @@ package com.skplanet.pandora.common;
 
 public class Constant {
 
-	public static final String UPLOADED_FILE_DIR = System.getProperty("user.home") + "/pandora-upload";
+	public static final String UPLOADED_FILE_DIR = System.getProperty("user.home") + "/.pandora";
 
 	public static final String PAGE_ID = "pageId";
 	public static final String USERNAME = "username";
