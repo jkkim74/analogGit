7762b83d4f7f81e8948607a7a8d1b9ed53652744
PP68961 <1600328@partner.skcc.com>
2016-09-01 13:28:37 +0900

    gulp order

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,22 @@
 
 var gulp = require('gulp');
 var inject = require('gulp-inject');
+var sort = require('gulp-sort');
 var mainBowerFiles = require('main-bower-files');
 
 gulp.task('inject', function () {
     var target = gulp.src('src/main/webapp/index.html');
     // It's not necessary to read the files (will speed up things), we're only after their paths: 
-    var sources = gulp.src(['src/main/webapp/partials/**/*.js', 'src/main/webapp/partials/**/*.css', 'src/main/webapp/styles/**/*.css', '!src/main/webapp/partials/**/*_test.js'], { read: false });
+    var sources = gulp.src([
+        '!src/main/webapp/partials/main.js', // manual import
+        'src/main/webapp/partials/**/*.js',
+        'src/main/webapp/partials/**/*.css',
+        'src/main/webapp/styles/**/*.css',
+        '!src/main/webapp/partials/**/*_test.js'
+    ]);
     var bowerComponents = gulp.src(mainBowerFiles(), { read: false });
 
-    return target.pipe(inject(sources, { ignorePath: 'src/main/webapp', addRootSlash: false }))
+    return target.pipe(inject(sources.pipe(sort()), { ignorePath: 'src/main/webapp', addRootSlash: false }))
         .pipe(inject(bowerComponents, { name: 'bower', ignorePath: 'src/main/webapp', addRootSlash: false }))
         .pipe(gulp.dest('src/main/webapp'));
 });
