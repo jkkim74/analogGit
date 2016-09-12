87f6ee38bc670c9d889759d1d9a94cd800e66f5b
PP68961 <1600328@partner.skcc.com>
2016-09-12 10:11:34 +0900

    delete legacy

--- src/main/java/com/skplanet/pandora/Application.java
+++ src/main/java/com/skplanet/pandora/Application.java
@@ -8,29 +8,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-	// @Override
-	// public void onStartup(ServletContext servletContext) throws
-	// ServletException {
-	// AnnotationConfigWebApplicationContext context = new
-	// AnnotationConfigWebApplicationContext();
-	// context.setConfigLocation("com.skplanet.pandora.configuration");
-	//
-	// servletContext.addListener(new ContextLoaderListener(context));
-	//
-	// ServletRegistration.Dynamic dispatcher =
-	// servletContext.addServlet("dispatcher",
-	// new DispatcherServlet(context));
-	// dispatcher.setLoadOnStartup(1);
-	// dispatcher.addMapping("/");
-	// dispatcher.setMultipartConfig(new
-	// MultipartConfigElement(System.getProperty("java.io.tmpdir")));
-	//
-	// FilterRegistration.Dynamic encodingFilter =
-	// servletContext.addFilter("encodingFilter",
-	// new CharacterEncodingFilter("UTF-8", true));
-	// encodingFilter.addMappingForUrlPatterns(null, false, "/*");
-	// }
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
