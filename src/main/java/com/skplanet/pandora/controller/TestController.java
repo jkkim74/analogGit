2775de6077b9e83a2b6de9932a8c13b9da1cfbe7
PP68961 <1600328@partner.skcc.com>
2016-09-20 14:11:28 +0900

    get client ip

--- src/main/java/com/skplanet/pandora/controller/TestController.java
+++ src/main/java/com/skplanet/pandora/controller/TestController.java
@@ -5,12 +5,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.skplanet.pandora.model.AutoMappedMap;
 import com.skplanet.pandora.model.UploadStatus;
@@ -72,6 +76,38 @@ public class TestController {
 		Map<String, Object> model = new HashMap<>();
 		model.put("msg", new Date());
 		smsService.send(new String[] { (String) params.get("to") }, "pan0104.vm", model);
+	}
+
+	@GetMapping("/testClientIp")
+	public Map<String, Object> testClientIp() {
+		HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+				.getRequest();
+		
+		String ip = req.getHeader("X-Forwarded-For");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getRemoteAddr();
+		}
+
+		// 여러 프록시를 경유했을 경우 콤마로 여러 IP가 누적됨. 맨 왼쪽 IP가 end-user's IP.
+		ip = ip.split(",")[0];
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("clientIp", ip);
+		map.put("getRemoteAddr()", req.getRemoteAddr());
+
+		return map;
 	}
 
 }
