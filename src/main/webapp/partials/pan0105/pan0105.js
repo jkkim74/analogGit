89b90a1ef039bfe6c48d80005799112a4028a631
PP68961 <1600328@partner.skcc.com>
2016-09-27 15:49:04 +0900

    fix upload bug

--- src/main/webapp/partials/pan0105/pan0105.js
+++ src/main/webapp/partials/pan0105/pan0105.js
@@ -32,45 +32,34 @@ angular.module('App')
         };
         $scope.altInputFormats = ['M!/d!/yyyy'];
 
-        $scope.gridOptionsPreview = {
-            enableColumnMenus: false,
-            enableSorting: false,
-            enableHorizontalScrollbar: uiGridConstants.scrollbars.NEVER,
-            minRowsToShow: 7,
-            columnDefs: [
-                { field: 'no', displayName: 'No.', width: 100, cellTemplate: '<div class="ui-grid-cell-contents">{{grid.renderContainers.body.visibleRowCache.indexOf(row) + 1}}</div>' },
-                { field: 'column1', displayName: 'Uploaded Data' }
-            ]
-        };
-
         $scope.gridOptionsMembers = {
             enablePaginationControls: false,
             paginationPageSize: 100,
             useExternalPagination: true,
             columnDefs: [
-                { field: 'rcvDt', displayName: '접수일시', cellTooltip: true, headerTooltip: true },
-                { field: 'rcvSeq', displayName: '접수번호', cellTooltip: true, headerTooltip: true },
-                { field: 'apprDttm', displayName: '승인일시', cellTooltip: true, headerTooltip: true },
-                { field: 'repApprNo', displayName: '대표 승인번호', cellTooltip: true, headerTooltip: true },
-                { field: 'apprNo', displayName: '승인번호', cellTooltip: true, headerTooltip: true },
-                { field: 'saleDttm', displayName: '매출일시', cellTooltip: true, headerTooltip: true },
-                { field: 'mbrId', displayName: '회원ID', cellTooltip: true, headerTooltip: true },
-                { field: 'cardDtlGrp', displayName: '카드코드', cellTooltip: true, headerTooltip: true },
-                { field: 'cardNo', displayName: '카드번호', cellTooltip: true, headerTooltip: true },
-                { field: 'alcmpn', displayName: '발생제휴사', cellTooltip: true, headerTooltip: true },
-                { field: 'mcnt', displayName: '발생가맹점', cellTooltip: true, headerTooltip: true },
-                { field: 'stlmtMcnt', displayName: '정산가맹점', cellTooltip: true, headerTooltip: true },
-                { field: 'pntKnd', displayName: '포인트종류', cellTooltip: true, headerTooltip: true },
-                { field: 'slip', displayName: '전표', cellTooltip: true, headerTooltip: true },
-                { field: 'saleAmt', displayName: '매출금액', cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
-                { field: 'pnt', displayName: '적립포인트', cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
-                { field: 'csMbrCmmsn', displayName: '제휴사연회비', cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
-                { field: 'cmmsn', displayName: '수수료', cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
-                { field: 'pmntWayCd', displayName: '지불수단', cellTooltip: true, headerTooltip: true },
-                { field: 'org', displayName: '기관', cellTooltip: true, headerTooltip: true },
-                { field: 'oilPrdctSgrp', displayName: '유종', cellTooltip: true, headerTooltip: true },
-                { field: 'saleQty', displayName: '주유량', cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
-                { field: 'cpnPrd', displayName: '쿠폰', cellTooltip: true, headerTooltip: true }
+                { field: 'rcvDt', displayName: '접수일시', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'rcvSeq', displayName: '접수번호', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'apprDttm', displayName: '승인일시', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'repApprNo', displayName: '대표 승인번호', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'apprNo', displayName: '승인번호', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'saleDttm', displayName: '매출일시', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'mbrId', displayName: '회원ID', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'cardDtlGrp', displayName: '카드코드', width: 300, cellTooltip: true, headerTooltip: true },
+                { field: 'cardNo', displayName: '카드번호', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'alcmpn', displayName: '발생제휴사', width: 300, cellTooltip: true, headerTooltip: true },
+                { field: 'mcnt', displayName: '발생가맹점', width: 300, cellTooltip: true, headerTooltip: true },
+                { field: 'stlmtMcnt', displayName: '정산가맹점', width: 300, cellTooltip: true, headerTooltip: true },
+                { field: 'pntKnd', displayName: '포인트종류', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'slip', displayName: '전표', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'saleAmt', displayName: '매출금액', width: 100, cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
+                { field: 'pnt', displayName: '적립포인트', width: 100, cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
+                { field: 'csMbrCmmsn', displayName: '제휴사연회비', width: 100, cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
+                { field: 'cmmsn', displayName: '수수료', width: 100, cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
+                { field: 'pmntWayCd', displayName: '지불수단', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'org', displayName: '기관', width: 300, cellTooltip: true, headerTooltip: true },
+                { field: 'oilPrdctSgrp', displayName: '유종', width: 100, cellTooltip: true, headerTooltip: true },
+                { field: 'saleQty', displayName: '주유량', width: 100, cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
+                { field: 'cpnPrd', displayName: '쿠폰', width: 100, cellTooltip: true, headerTooltip: true }
             ],
             onRegisterApi: function (gridApi) {
                 $scope.gridApi = gridApi;
@@ -89,16 +78,16 @@ angular.module('App')
 
             $scope.uploadPromise = uploadService.upload({ file: file, columnName: $scope.selectedOption2.value });
             $scope.uploadPromise.then(function () {
-                self.checkUploadProgress();
-                $scope.uploadProgressLoadingMessage = 'Loading...';
+                $scope.uploadProgressLoadingMessage = 'Extracting...';
 
-                return uploadService.getUploadedPreview();
-            }, null, function (progressPercentage) {
-                $scope.uploadProgressLoadingMessage = 'Uploading...' + progressPercentage + '%';
-            }).then(function (data) {
-                $scope.gridOptionsPreview.data = data.value;
-
-                $scope.extractMemberInfo();
+                return apiService.extractMemberInfo({
+                    inputDataType: $scope.selectedOption.value,
+                    periodType: $scope.selectedOption3.value,
+                    periodFrom: $scope.periodFrom,
+                    periodTo: $scope.periodTo
+                });
+            }).then(function () {
+                $scope.extracted = true;
             });
         };
 
@@ -113,37 +102,10 @@ angular.module('App')
             });
         };
 
-        self.checkUploadProgress = function () {
-            uploadService.getUploadProgress().finally(function (data) {
-                $scope.uploadProgress = false;
-            }, function (totalRecords) {
-                $scope.uploadProgress = true;
-                $scope.uploadedRecords = totalRecords;
-            });
-        };
-
-        // 이전 업로드가 진행중이라면 표시.
-        // self.checkUploadProgress();
-
-
         $scope.sendPts = function (ptsUsername, ptsMasking) {
             $scope.sendPtsPromise = apiService.sendPts({ ptsUsername: ptsUsername, ptsMasking: !!ptsMasking });
             $scope.sendPtsPromise.finally(function () {
 
-            });
-        };
-
-        $scope.extractMemberInfo = function () {
-            var params = {
-                inputDataType: $scope.selectedOption.value,
-                periodType: $scope.selectedOption3.value,
-                periodFrom: $scope.periodFrom,
-                periodTo: $scope.periodTo
-            };
-
-            $scope.extractPromise = apiService.extractMemberInfo(params);
-            $scope.extractPromise.then(function () {
-                $scope.extracted = true;
             });
         };
 
