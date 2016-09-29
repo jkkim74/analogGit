5e994f6985e06a4e390b53fe5913bb422bfc163a
PP68961 <1600328@partner.skcc.com>
2016-09-29 10:59:02 +0900

    미사용 코드 삭제

--- src/main/java/com/skplanet/pandora/util/Helper.java
+++ src/main/java/com/skplanet/pandora/util/Helper.java
@@ -1,19 +1,8 @@
 package com.skplanet.pandora.util;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.Locale;
 import java.util.UUID;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.format.datetime.DateFormatter;
 
@@ -33,37 +22,6 @@ public final class Helper {
 			return prefix + '_' + nowDateString() + '_' + UUID.randomUUID() + ".csv";
 		}
 		return nowDateString() + '_' + UUID.randomUUID() + ".csv";
-	}
-
-	private static final String ENC_KEY = "0123456789abcdef";
-	private static final String ENC_IV = "AAAAAAAAAAAAAAAA";
-
-	public static byte[] aesEncrypt(String plainText) {
-		try {
-			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			SecretKeySpec key = new SecretKeySpec(ENC_KEY.getBytes("UTF-8"), "AES");
-			cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(ENC_IV.getBytes("UTF-8")));
-			byte[] bytes = cipher.doFinal(plainText.getBytes("UTF-8"));
-
-			return bytes;
-
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | UnsupportedEncodingException | InvalidKeyException
-				| InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public static String aesDecrypt(byte[] cipherText) {
-		try {
-			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			SecretKeySpec key = new SecretKeySpec(ENC_KEY.getBytes("UTF-8"), "AES");
-			cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(ENC_IV.getBytes("UTF-8")));
-			return new String(cipher.doFinal(cipherText), "UTF-8");
-
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | UnsupportedEncodingException | InvalidKeyException
-				| InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 	public static String skpEncrypt(String plainText) {
