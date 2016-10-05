fd1d66452ca2ce3c3c43347364d8fd87a5321ff4
PP68961 <1600328@partner.skcc.com>
2016-10-05 17:54:34 +0900

    fix bug

--- src/main/java/com/skplanet/pandora/service/UserService.java
+++ src/main/java/com/skplanet/pandora/service/UserService.java
@@ -116,7 +116,7 @@ public class UserService implements UserDetailsService {
 	}
 
 	public void updateAccesses(String username, String pageList) {
-		mysqlRepository.deleteAccesses(username);
+		mysqlRepository.deleteAccesses(username, false);
 
 		if (!StringUtils.isEmpty(pageList)) {
 			mysqlRepository.insertAccesses(username, pageList);
@@ -126,8 +126,10 @@ public class UserService implements UserDetailsService {
 	public void updateAdmin(String username, boolean isAdmin) {
 		if (isAdmin) {
 			mysqlRepository.insertAuthorities(username, "ROLE_ADMIN");
+			mysqlRepository.insertAccesses(username, "PAN0002,PAN0003");
 		} else {
 			mysqlRepository.deleteAuthorities(username, "ROLE_ADMIN");
+			mysqlRepository.deleteAccesses(username, true);
 		}
 	}
 
