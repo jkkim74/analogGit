65d0516812f813a8a3090d7501ab47b2cfdc609f
PP68961 <1600328@partner.skcc.com>
2016-10-06 15:26:10 +0900

    나눔고딕 폰트 설정

--- src/main/webapp/partials/ctas0101/ctas0101.js
+++ src/main/webapp/partials/ctas0101/ctas0101.js
@@ -4,7 +4,21 @@ angular.module('App')
     .controller('Ctas0101Ctrl', ['$scope', '$q', '$http', '$timeout', 'uiGridConstants', 'toastr', 'apiSvc', function ($scope, $q, $http, $timeout, uiGridConstants, toastr, apiSvc) {
 
         var self = this;
-
         $scope.title = '이메일 발송 관리';
+
+        $scope.gridOptionsList = {
+            useExternalPagination: true,
+            enableHorizontalScrollbar: uiGridConstants.scrollbars.NEVER,
+            columnDefs: [
+                { field: 'no', displayName: 'No.', width: 100, cellTemplate: '<div class="ui-grid-cell-contents">{{grid.renderContainers.body.visibleRowCache.indexOf(row) + 1}}</div>' },
+                { field: 'column1', displayName: 'Uploaded Data' }
+            ],
+            onRegisterApi: function (gridApi) {
+                $scope.gridApi = gridApi;
+                gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
+                    $scope.loadMembers();
+                });
+            }
+        };
 
     }]);
