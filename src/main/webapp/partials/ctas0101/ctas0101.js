e5ec6d4e5dff33ea8db67e523cc2f1ee84b4a7bf
PP68961 <1600328@partner.skcc.com>
2016-10-07 11:23:19 +0900

    CTAS: 이메일발송관리 화면

--- src/main/webapp/partials/ctas0101/ctas0101.js
+++ src/main/webapp/partials/ctas0101/ctas0101.js
@@ -6,19 +6,64 @@ angular.module('App')
         var self = this;
         $scope.title = '이메일 발송 관리';
 
+        $scope.visibleDetail = true;
+
+        $scope.selectOptions = [
+            { label: '커머스센터 대용량 메일', value: 'em' },
+            { label: '앱푸시/모바일전용 회원ID 전송 (PTS)', value: 'push' },
+            { label: 'SMS 발송용 목록 전송 (PTS)', value: 'sms' }
+        ];
+
         $scope.gridOptionsList = {
             useExternalPagination: true,
             enableHorizontalScrollbar: uiGridConstants.scrollbars.NEVER,
+            minRowsToShow: 16,
             columnDefs: [
-                { field: 'no', displayName: 'No.', width: 100, cellTemplate: '<div class="ui-grid-cell-contents">{{grid.renderContainers.body.visibleRowCache.indexOf(row) + 1}}</div>' },
-                { field: 'column1', displayName: 'Uploaded Data' }
+                { field: 'no', displayName: 'No.', width: 50, cellTemplate: '<div class="ui-grid-cell-contents">{{grid.renderContainers.body.visibleRowCache.indexOf(row) + 1}}</div>' },
+                { field: 'cpmnCd', displayName: '캠페인코드', cellTooltip: true, headerTooltip: true },
+                { field: 'cpmnNm', displayName: '캠페인명', cellTooltip: true, headerTooltip: true },
+                { field: 'sndChnl', displayName: '발송채널', cellTooltip: true, headerTooltip: true },
+                { field: 'mrgDt', displayName: '머지일자', cellTooltip: true, headerTooltip: true },
+                { field: 'sndDt', displayName: '발송일자', cellTooltip: true, headerTooltip: true },
+                { field: 'requester', displayName: '요청담당자', cellTooltip: true, headerTooltip: true },
+                { field: 'sndSts', displayName: '상태', cellTooltip: true, headerTooltip: true }
             ],
             onRegisterApi: function (gridApi) {
                 $scope.gridApi = gridApi;
                 gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
-                    $scope.loadMembers();
+                    var offset = (newPage - 1) * pageSize;
+                    $scope.search(offset, pageSize);
                 });
             }
+        };
+
+        $scope.search = function (offset, limit) {
+            var params = {
+                offset: offset || 0,
+                limit: limit || $scope.gridOptionsList.paginationPageSize
+            };
+
+            // $scope.searchPromise = apiSvc.getCampaigns(params);
+            // $scope.searchPromise.then(function (data) {
+            //     $scope.gridOptionsList.data = data.value;
+            //     $scope.gridOptionsList.totalItems = data.totalRecords;
+            // });
+        };
+
+
+        $scope.gridOptionsCellList = {
+            enableHorizontalScrollbar: uiGridConstants.scrollbars.NEVER,
+            minRowsToShow: 5,
+            columnDefs: [
+                { field: 'cpmnCd', displayName: '셀ID', width: 50, cellTooltip: true, headerTooltip: true },
+                { field: 'cpmnNm', displayName: '설명', cellTooltip: true, headerTooltip: true },
+                { field: 'sndChnl', displayName: '백분율(%)', width: 50, cellTooltip: true, headerTooltip: true },
+                { field: 'mrgDt', displayName: '추출수', width: 100, cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
+                { field: 'sndDt', displayName: '최종추출수', width: 100, cellTooltip: true, headerTooltip: true, cellFilter: 'number' },
+                { field: 'requester', displayName: '머지일자', cellTooltip: true, headerTooltip: true },
+                { field: 'sndSts', displayName: '발송일자', cellTooltip: true, headerTooltip: true },
+                { field: 'sndSts', displayName: '셀상태', width: 100, cellTooltip: true, headerTooltip: true }
+            ]
         };
 
     }]);
