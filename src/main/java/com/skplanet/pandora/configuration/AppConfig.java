85c15c00d9902258f0c1624b5fbb090566fbbccf
PP68961 <1600328@partner.skcc.com>
2016-10-10 15:03:44 +0900

    fix to bug

--- src/main/java/com/skplanet/pandora/configuration/AppConfig.java
+++ src/main/java/com/skplanet/pandora/configuration/AppConfig.java
@@ -2,11 +2,14 @@ package com.skplanet.pandora.configuration;
 
 import java.util.concurrent.Executor;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -15,10 +18,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableScheduling
 @EnableAuthorizationServer
 @EnableResourceServer
-public class AppConfig extends AsyncConfigurerSupport {
+public class AppConfig extends AsyncConfigurerSupport implements SchedulingConfigurer {
 
 	@Override
 	public Executor getAsyncExecutor() {
+		return taskExecutor();
+	}
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		taskRegistrar.setScheduler(taskExecutor());
+	}
+
+	@Bean(destroyMethod = "shutdown")
+	public Executor taskExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 		executor.setCorePoolSize(7);
 		executor.setMaxPoolSize(42);
