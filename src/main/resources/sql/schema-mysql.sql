5a021f1ee93cb3cb1938836e930fdf14afcc8722
PP68961 <1600328@partner.skcc.com>
2016-10-11 11:06:41 +0900

    테스트 DB 스키마 수정

--- src/main/resources/sql/schema-mysql.sql
+++ src/main/resources/sql/schema-mysql.sql
@@ -34,15 +34,3 @@ CREATE TABLE PAGE_ACCESS (
 	FOREIGN KEY(USERNAME) REFERENCES USERS(USERNAME)
 );
 CREATE UNIQUE INDEX IX_PAGE_USERNAME ON PAGE_ACCESS (USERNAME, PAGE_ID);
-
-insert into users(username, password, enabled) values('user', 'user', 1);
-insert into authorities(username, authority) values('user', 'ROLE_USER');
-insert into authorities(username, authority) values('user', 'ROLE_ADMIN');
-insert into page_access(username, page_id) values('user', 'PAN0101');
-insert into page_access(username, page_id) values('user', 'PAN0102');
-insert into page_access(username, page_id) values('user', 'PAN0103');
-insert into page_access(username, page_id) values('user', 'PAN0104');
-insert into page_access(username, page_id) values('user', 'PAN0105');
-insert into page_access(username, page_id) values('user', 'PAN0106');
-insert into page_access(username, page_id) values('user', 'PAN0002');
-insert into page_access(username, page_id) values('user', 'PAN0003');
