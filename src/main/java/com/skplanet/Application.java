5b13ef13f7d681828fc846c87871f90706a90377
PP68961 <1600328@partner.skcc.com>
2016-10-27 13:27:23 +0900

    로컬 테스트 실행환경 수정

--- src/main/java/com/skplanet/Application.java
+++ src/main/java/com/skplanet/Application.java
@@ -1,6 +1,5 @@
 package com.skplanet;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -11,10 +10,6 @@ public class Application extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
-	}
-
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ src/test/java/com/skplanet/ApplicationTestLocal.java
@@ -0,0 +1,11 @@
+package com.skplanet;
+
+import org.springframework.boot.SpringApplication;
+
+public class ApplicationTestLocal {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
